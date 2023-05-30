1301ccf0c7a85985b0e0210c19a3c7489bf1e179
vishalinagathevan <vishali.nagathevan@gmail.com>
2023-05-30 07:15:33 +0530

    Basic mommits for git

---
+++ GitCommants.py
@@ -0,0 +1,7 @@
+# USING GIT COMMANDS :
+# Basic commands 
+first_step ="git add ."
+second_step ="git status"
+thirt_step ="git commit -m'message'"
+final_step ="git push"
+print("GIT BASIC COMMANTS",final_step,second_step,thirt_step,final_step)
